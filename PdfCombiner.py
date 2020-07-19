aba93aaaec82792e5cdb5eca4a603b0f7f5392e5
DJKC <lasjapas@yahoo.com>
2020-07-19 04:38:20 -0700

    Wrote reverse pdf and merge pdf functions. Need to create folder for outputs and adjust reader for files with different name. I've learned alot in this one sitting.

--- PdfCombiner.py
+++ PdfCombiner.py
@@ -1,8 +1,49 @@
-# Import necessary modules
-import os, PyPDF2 as pypdf, logging
-from pathlib import Path
+# https://programtalk.com/python-examples/PyPDF2.PdfFileMerger/
+
+import os, PyPDF2, logging
 logging.basicConfig(level=logging.DEBUG)
-# logging.disable(logging.DEBUG)
+logging.disable(logging.DEBUG)
+
+
+def get_merged_pdf(fileList, outputPDF):
+    merger = PyPDF2.PdfFileMerger()
+
+    pdfs = [open(pdf, 'rb') for pdf in fileList]
+
+    for pdf in pdfs:
+        merger.append(pdf)
+
+    outFile = outputPDF
+
+    with open(outFile, 'wb') as pdf:
+        merger.write(pdf)
+
+    [fp.close() for fp in pdfs]
+    print(outputPDF + " has been saved.")
+    merger.close()
+
+
+def get_reverse_pdf(fileName, outputPDF = ""):
+    if outputPDF == "":
+        outputPDF = fileName
+
+    pdf = open(fileName, "rb")
+    reader = PyPDF2.PdfFileReader(pdf)
+    writer = PyPDF2.PdfFileWriter()
+
+    for page in range(reader.getNumPages() - 1, -1, -1):
+        page = reader.getPage(page)
+        writer.addPage(page)
+
+    newPDF = open(outputPDF, "wb")
+    writer.write(newPDF)
+
+    newPDF.close()
+    newPDF.close()
+    pdf.close()
+
+    print(fileName, "has been reversed.")
+
 
 folders = []
 folderBase = "/Users/X/Uncompressed"
@@ -21,11 +62,9 @@ for name in folders:
 
 logging.debug("-------------------")
 
-# The folder name will be the name of the resulting pdf
-# Open folders
 for name in folders:
     os.chdir(folderBase + '/' + name)
-    files = {"FileName": [], "PdfObject": [], "BookNumber": [], "StartPage": [], "EndPage": [], "EvenOdd": []}
+    files = {"FileName": []}
 
     for file in os.listdir():
         if os.path.isfile(file) and file.endswith(".pdf"):
@@ -33,12 +72,12 @@ for name in folders:
 
     if name == "YellowPacket":  # Files are organized differently, needs special care
 
-        writer = pypdf.PdfFileWriter()
+        writer = PyPDF2.PdfFileWriter()
 
         for file in ["YellowPacket.pdf", "YellowPacket0001.pdf", "YellowPacket0002.pdf", "YellowPacket0003.pdf", "YellowPacket0004.pdf"]:
             pdf_file = open(file, "rb")
             logging.debug("Working on packet " + str(file))
-            reader = pypdf.PdfFileReader(file)
+            reader = PyPDF2.PdfFileReader(file)
             for pageNum in range(reader.numPages):
                 pageNum = reader.getPage(pageNum)
                 writer.addPage(pageNum)
@@ -49,49 +88,57 @@ for name in folders:
         pdf_file.close()
 
     else:
-        fileList = even = odd =[]
-        count = 0
+        fileList = []
+        even = []
+        odd = []
 
         fileList = os.listdir(folderBase + '/' + name)
-        # del fileList[fileList.index(".DS_Store")]
+
+        if os.path.exists(".DS_Store"):
+            del fileList[fileList.index(".DS_Store")]
 
         for file in fileList:
+            originalName = file
             file = list(file)
+
+            if 'T' in file:
+                remove = file.index('T')
+                del file[remove: remove + 2]
+
+                for i in range(2, 5):
+                    file.insert(remove, '0')
+
             while file.index('-') != 5:
                 file.insert(2, '0')
 
+            file = "".join(file)
+
+            os.rename(originalName, file)
+
             if "_O" in file:
-                file = file.replace("TO", "000")
                 odd.append(file)
-            elif "_E" in fileList:
-                file = file.replace("TE", "000")
+            elif "_E" in file:
                 even.append(file)
 
         odd.sort(key = lambda x: x[1:5])
-        even.sort(key = lambda x: x[1:5])
+        merge_pdfs(odd, name + "_odd.pdf")
 
-        print(odd, end = "\n----------\n")
-        print(even)
+        even.sort(key = lambda x: x[1:5], reverse = True)
+        merge_pdfs(even, name + "_even.pdf")
 
-        for file in os.listdir(folderBase + '/' + name):
-            newfile = file.replace("TO", "0")
-            newfile = file.replace("TE", "0")
-            fileList.append
-
-            if "_O" in file:
+        merge_pdfs(["Book1_odd.pdf", "Book1_even.pdf"], "Book1_Complete.pdf")
 
 
-            if "_E" in file:
-                newfile = file[:(file.index("_E") + 2)] + ".pdf"
-                newfile = newfile.replace('-', '_')
-                newfile = newfile.replace("TE", "0")
-                os.rename(file, newfile)
-                even[0].append(newfile)
-                split = even[0][count].split('_')
-
-                if split[1] == "TE":
-                    split[1] = 0
-
-                even[1].append(int(split[1]))
-                even[2].append(int(split[2]))
-    print("\n-----------------------------------------------------------------")
+#    print("\n-----------------------------------------------------------------")
+#
+# https://stackoverflow.com/questions/17104926/pypdf-merging-multiple-pdf-files-into-one-pdf
+#
+# from PyPDF2 import PdfFileMerger, PdfFileReader
+#
+# [...]
+#
+# merger = PdfFileMerger()
+# for filename in filenames:
+#     merger.append(PdfFileReader(file(filename, 'rb')))
+#
+# merger.write("document-output.pdf")
