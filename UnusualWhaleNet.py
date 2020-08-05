259ba6d17277ab165908fbdca86c0ab299ee7ee4
DJKC <lasjapas@yahoo.com>
2020-08-05 15:56:07 -0700

    Working on the regex to parse the elements of each plays.

--- UnusualWhaleNet.py
+++ UnusualWhaleNet.py
@@ -1,56 +1,114 @@
-import requests, bs4 as bs, re, sys, os, openpyxl
+import requests
+from io import StringIO
+import bs4 as bs
+import re
+import sys
+import os
+import openpyxl
+import sqlite3
 
-os.chdir("/Users/X/UnusualWhales_6-16_Present")
+def resetSD(strikeData):
+    for i in strikeData:
+        strikeData[i] = []
 
-if len(sys.argv) >= 1:
-    argList = []
+os.chdir("/Users/X/UnusualWhalesHistory/")
+smallList = "06-16-20.html"
+rawplays = []
+argList = []
+plays = []
 
-    for arg in sys.argv[1:]:
+strikeData = {"Symbol": [],
+              "Date": [],
+              "Style": [],
+              "Strike": [],
+              "Bid": [],
+              "Ask": [],
+              "OpenInterest": [],
+              "Volume": [],
+              "IV": [],
+              "ChangePercent": [],
+              "ChangeCost": [],
+              }
+
+# Read the command line arguments and added file names to list
+if len(sys.argv) > 1:
+    print("CMD args accepted: ", sys.argv)
+
+    for arg in sys.argv[:]:
+        print(arg)
         argList.append(arg)
 
+# If this is being run from the IDE
 else:
-    argList = os.listdir()
+    # argList = os.listdir()
+    # Will be set to a certain file while practicing.
+    argList.append(smallList)
+    print("Running from IDE: ", argList)
 
-for inputFile in sys.argv[1:]:
+[print("Arg: ", item) for item in sys.argv]
+[print(item) for item in argList]
+
+rawplays = []
+
+#   The strikes in the files will be added to a database if they don't exist already.
+for inputFile in argList:
     htmlData = open(str(inputFile), 'r')
     contents = htmlData.read()
     soup = bs.BeautifulSoup(contents, "html.parser")
 
     # For the strike
-    strikePlays = soup.find_all("a", {"class": "chatlog__embed-title-link"})
-    # For the strike details
-    strikeDetails = soup.find_all("div", {"class": "chatlog__embed-description"})
+    strikePlays = soup.find_all("div", {"class": "markdown"})
 
-#   1398 Automate has excel help
-#    wb = opxl.Workbook()
+    # Removes img tag
+    for img in soup("img"):
+        if isinstance(img, bs.element.Tag):
+            img.decompose()
+
+    # Gets the text without the div tag
+    for div in strikePlays:
+        rawplays.append(div.text)
+        print(div.text)
+
+    for i in range(len(rawplays)):
+        while not rawplays[i][0].isalpha():
+            rawplays[i] = rawplays[i][1:]
+
+        if not rawplays[i].endswith('\n'):
+            rawplays[i] += '\n'
+
+    # Prints the value in each dict item for number v
 
     # For every item:
     #   if it doesn't exist in file:
-    #       Write to excel file
+    #       Write to excel file or text or sql
 
-    for item in strikePlays:
-        print(item.div.text)
+    pattern_text = re.compile(r'''(?P<Symbol>\w{1,5})\s*
+                                    (?P<Date>\d{4}-\d{2}-\d{2})\s*
+                                    (?P<Style>\w{1})\s*\$
+                                    (?P<Strike>\d{2,5}(\.\d{2})?)\s*Bid:\s*\$
+                                    (?P<Bid>\d{1,5}(\.\d{2})?)\s*Bid:\s*\$
+                                    (?P<Ask>\d{1,5}(\.\d{2})?)\s*Interest:\s*
+                                    (?P<Interest>\d{1,7})\s*Volume:\s*
+                                    (?P<Volume>\d{1,7})\s*IV:\s*
+                                    (?P<IV>\d{1,4}(\.\d{1,2})?)%\s*%\s*Diff:\s*
+                                    (?P<Diff>\d{1,4}(\.\d{1,2})?)%\s*Purchase:\s*\$
+                                    (?P<Price>\d{1,4}(\.\d{1,2})?)
+                                    ''')
 
-    for item in strikeDetails:
-        print(item.div.text)
+    for play in rawplays:
+        all = pattern_text.finditer(play)
 
-# Create output file to store data
-# Add data to excel sheet if it doesnt already exist.
+        for match in all:
+            print(match)
 
+'''
+IQ 2020-07-17 P $25
 
-# The parent branch that holds both the strike abd details branch
-# <div id="mess-\d16>...
-# Hold the Strike Play info
-# <a class="chatlog__embed-title-link"><div class="markdown">.text
-#   ($name 2020-07-21 P $11)
-# Holds the strike details info
-# <div class="chatlog__embed-description"><div class="markdown>.text
-#   (️ ignore whitespace here
-#
-# Bid: $1.41
-# Ask: $1.63
-# Interest: 5
-# Volume: 550
-# IV: 87.61%
-# % Diff: -23.66%
-# Purchase: $17.03)
+Bid: $2.94
+Ask: $2.99
+Interest: 6
+Volume: 447
+IV: 73.12%
+% Diff: 6.7%
+Purchase: $23.43
+'''
