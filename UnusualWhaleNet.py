4a829583fc1baab0d8497bf31f45f90a994e1719
DJKC <lasjapas@yahoo.com>
2020-08-06 03:03:42 -0700

    Problems occur when using a file 100 larger than sample, not surprised, :/.

--- UnusualWhaleNet.py
+++ UnusualWhaleNet.py
@@ -12,24 +12,27 @@ def resetSD(strikeData):
         strikeData[i] = []
 
 os.chdir("/Users/X/UnusualWhalesHistory/")
-smallList = "06-16-20.html"
+smallList = "07-01-20_07-10-20.html"
 rawplays = []
 argList = []
 plays = []
 valid_symbols = "!@#$%^&*()_-+={}[]"
 
-strikeData = {"Symbol": [],
-              "Date": [],
-              "Style": [],
-              "Strike": [],
-              "Bid": [],
-              "Ask": [],
-              "OpenInterest": [],
-              "Volume": [],
-              "IV": [],
-              "ChangePercent": [],
-              "ChangeCost": [],
-              }
+strikeData = []
+strikeInfo = ["Symbol",
+              "Date",
+              "Style",
+              "Strike",
+              "Bid",
+              "Ask",
+              "Interest",
+              "Volume",
+              "IV",
+              "Diff",
+              "Price",
+              "ChangePercent",
+              "ChangeCost",
+              ]
 
 # Read the command line arguments and added file names to list
 if len(sys.argv) > 1:
@@ -74,30 +77,46 @@ for inputFile in argList:
         strikeData.append(string)
 
     for i in range(len(strikeData)):
+        print('[', i, ']: ', strikeData[i])
+
+        # IndexError: list index out of range
+        # [ 178 ]:  []
+
         while not strikeData[i][0].isalpha():
             del strikeData[i][0]
 
         strikeData[i] = ''.join(strikeData[i])
         print(strikeData[i])
 
-    # Prints the value in each dict item for number v
-
-    # For every item:
-    #   if it doesn't exist in file:
-    #       Write to excel file or text or sql
-
     pattern_text = re.compile(r'''(?P<Symbol>\w{1,5})\s*
                                     (?P<Date>\d{4}-\d{2}-\d{2})\s*
                                     (?P<Style>\w{1})\s*\$
-                                    (?P<Strike>\d{2,5}(\.\d{2})?)\s*Bid:\s*\$
-                                    (?P<Bid>\d{1,5}(\.\d{2})?)\s*Bid:\s*\$
-                                    (?P<Ask>\d{1,5}(\.\d{2})?)\s*Interest:\s*
+                                    (?P<Strike>\d{2,5}(\.\d{1,2})?)\s*Bid:\s*\$
+                                    (?P<Bid>\d{1,5}(\.\d{1,2})?)\s*Ask:\s*\$
+                                    (?P<Ask>\d{1,5}(\.\d{1,2})?)\s*Interest:\s*
                                     (?P<Interest>\d{1,7})\s*Volume:\s*
                                     (?P<Volume>\d{1,7})\s*IV:\s*
                                     (?P<IV>\d{1,4}(\.\d{1,2})?)%\s*%\s*Diff:\s*
                                     (?P<Diff>-?\d{1,4}(\.\d{1,2})?)%\s*Purchase:\s*\$
                                     (?P<Price>\d{1,4}(\.\d{1,2})?)
-                                    ''')
+                                    ''', re.VERBOSE)
+
+    # match = pattern_text.match(strikeData[0])
+    # match.group("Date")
+
+    for i in range(len(strikeData)):
+        match = pattern_text.match(strikeData[i])
+
+        for j in range(11):
+            print(match.group(strikeInfo[j]))
+
+        print('*' * 20)
+
+    # Prints the value in each dict item for number v
+
+    # For every item:
+    #   if it doesn't exist in file:
+    #       Write to excel file or text or sql
 
     for play in rawplays:
         all = pattern_text.finditer(play)
